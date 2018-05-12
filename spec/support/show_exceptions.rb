4afe2e238f1d8e8a12a862901edeaf87b24cbc40
Andrew White <andrew.white@unboxed.co>
2018-05-12 19:03:50 +0100

    Move show exceptions helper to an anonymous module
    Also turn off the debugging error page and render public error pages.

--- spec/support/show_exceptions.rb
+++ spec/support/show_exceptions.rb
@@ -1,19 +1,22 @@
-module ShowExceptionsHelper
-  def with_show_exceptions(on_or_off, &block)
-    begin
-      env_config = Rails.application.env_config
-      show_exceptions = env_config['action_dispatch.show_exceptions']
-      env_config['action_dispatch.show_exceptions'] = on_or_off
+RSpec.configure do |config|
+  helper = Module.new do
+    def with_show_exceptions(on_or_off, &block)
+      begin
+        env_config = Rails.application.env_config
+        show_exceptions = env_config['action_dispatch.show_exceptions']
+        show_detailed_exceptions = env_config['action_dispatch.show_detailed_exceptions']
+        env_config['action_dispatch.show_exceptions'] = on_or_off
+        env_config['action_dispatch.show_detailed_exceptions'] = !on_or_off
 
-      yield
-    ensure
-      env_config['action_dispatch.show_exceptions'] = show_exceptions
+        yield
+      ensure
+        env_config['action_dispatch.show_exceptions'] = show_exceptions
+        env_config['action_dispatch.show_detailed_exceptions'] = show_detailed_exceptions
+      end
     end
   end
-end
 
-RSpec.configure do |config|
-  config.include(ShowExceptionsHelper, type: :request)
+  config.include(helper, type: :request)
 
   config.around(:each, type: :request) do |example|
     if example.metadata.key?(:show_exceptions)
